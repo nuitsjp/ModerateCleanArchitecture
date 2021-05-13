a9527532ebea289947086978baa19af8419c94a3
Atsushi Nakamura <nuits.jp@live.jp>
2021-05-14 07:09:21 +0900

    gitignoreの修正

---
+++ Strong/AdventureWorks.UseCase.Export/IExportService.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.UseCase.Export
+{
+    public interface IExportService
+    {
+        Task ExportAsync(ISalesOrderDetailKey salesOrderDetailKey, TextWriter textWriter);
+    }
+}
